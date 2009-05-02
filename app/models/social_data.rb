ae0b49027a7bda1d28a8fd3510cdce135554b262
Kane Baccigalupi <baccigalupi@gmail.com>
2009-05-02 14:47:05 -0700

    moving auth off to a separate database

---
+++ app/models/social_data.rb
@@ -0,0 +1,4 @@
+class SocialData < ActiveRecord::Base
+  self.abstract_class = true
+  establish_connection( "people_#{RAILS_ENV}" ) 
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
-class User < ActiveRecord::Base
+class User < SocialData
   include Clearance::User
 end
