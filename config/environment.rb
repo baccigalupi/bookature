a67bbda174032143f9daaf00cd79fd42a9e21034
Kane Baccigalupi <baccigalupi@gmail.com>
2009-05-02 13:16:48 -0700

    clearance auth engin

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,9 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :version => '~> 2.3.8', 
     :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem 'RedCloth'
+  config.gem 'thoughtbot-clearance', :version => '0.6.2',  
+    :lib => 'clearance', :source => 'http://gems.github.com' 
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
